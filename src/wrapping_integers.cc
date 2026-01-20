3eb49595921fb105b0c6d0e51417938bdfe8104b
zrxrz <lalunelqm@outlook.com>
2026-01-20 05:20:49 +0000

    Lab Checkpoint 2: Wrap32

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -1,18 +1,20 @@
 #include "wrapping_integers.hh"
 #include "debug.hh"
+#include <cstdint>
 
 using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
   // Your code here.
-  debug( "unimplemented wrap( {}, {} ) called", n, zero_point.raw_value_ );
-  return Wrap32 { 0 };
+  return zero_point + static_cast<uint32_t>(n);
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  debug( "unimplemented unwrap( {}, {} ) called", zero_point.raw_value_, checkpoint );
-  return {};
+  // your code here.
+  Wrap32 ckpt_32{wrap(checkpoint, zero_point)};
+  int32_t signed_diff{static_cast<int32_t>(this->raw_value_ - ckpt_32.raw_value_)};
+  int64_t result{static_cast<int64_t>(signed_diff) + static_cast<int64_t>(checkpoint)};
+  return result < 0 ? result + (1ULL << 32) : result;
 }
